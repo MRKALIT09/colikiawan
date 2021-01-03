f8a21f32c5c488c2b99bbb68c260319d72eee784
MR_KALIT09 <75316440+MRKALIT09@users.noreply.github.com>
2021-01-04 01:23:14 +0700

    Add files via upload

---
+++ Tobat.sh
@@ -0,0 +1,147 @@
+#!/system/xbin/bash
+#Jangan di recode dong ster 
+#usaha kalo gk bisa nanya 
+
+
+blue='\033[34;1m'
+green='\033[32;1m'                                        
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+echo $red "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $blue "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $green "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $red "Tobat Goblok Jangan Coli Mulu :V"
+sleep 
+clear
+echo $blue "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $green "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $red "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $blue "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+echo $green "Tobat Goblok Jangan Coli Mulu :V"
+sleep 1
+clear
+
+echo "\033[1;31m]
+                /^...^
+               /<_* *_>   
+              /\/ \_/ 
+      ___.-._/\/
+     /=*_*-*-*/!
+    |-{-_-_-} ! 
+    (-{-_-_-} ! 
+     \{_-_-_} ! 
+      }-_-_-} 
+      {-_|-_} 
+      {-_|_-} 
+      {_-|-_} 
+      {_-|-_} 
++=====%%@ @%%=============================+
++ coded by : InYourG00D                   +
++ Team : LNX#CREW S.T.C BUFT              +
++=========================================+
+  ^...^                      ^...^
+ <_* *_>Tools Install Bokep <_* *_> 
+   \_/                        \_/   ";
+echo $blue "-=[Menu]=-"
+echo $green "[================================[>"
+echo "1.Jepang Toge"|lolcat
+echo $red "[================================[>"
+echo "2.Indo Jilbab hot"|lolcat
+echo $yellow "[================================[>"
+echo "3.SMA Toge"|lolcat
+echo $green "[================================[>"
+echo "4.Semok"|lolcat
+echo $red "[================================[>"
+echo "5.Jepang montoq"|lolcat
+echo $yellow "[================================[>"
+echo "6.SMA "|lolcat
+echo $green "[================================[>"
+echo "00.Ini Out"|lolcat
+echo $red
+read -p "[>>" gmt
+
+if [ $gmt = 1 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0029.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 2 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0103.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 3 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0108.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 4 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0104.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+echo $blue "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 5 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0106.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 6 ]
+then 
+clear
+echo "Wait Bro"
+cd menu
+mv -f VID-20190311-WA0100.mp4 /sdcard
+echo $red "DoneBosQ"
+echo $green "Silahkan Cek Di Folder/file"
+fi
+
+if [ $gmt = 00 ]
+then 
+clear 
+echo "Bye Happy Cloy"|lolcat
+exit
+fi
